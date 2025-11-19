1ed29294103fd67efb64398fd1fcd5c0df00e5f4
akshaywagh3 <akshayw3121@gmail.com>
2025-11-19 12:08:01 +0530

    change

--- backend/src/services/chatService.js
+++ backend/src/services/chatService.js
@@ -4,7 +4,7 @@ import ErrorHandler from "../middleware/errorMiddleware.js";
 
 class Chatservice{
     constructor() {
-        this.workspaceRepo = new WorkspaceRepository();
+        this.ChatRepo = new ChatRepository();
         this.messageRepo = new MessageRepository();
     }
 
@@ -14,21 +14,51 @@ class Chatservice{
     }
 
     async createGroupChat(name, members, workspaceId) {
+        if(!name)  throw ErrorHandler('Name is required',400);
+
+        if(!members || members.length <2) throw ErrorHandler('Atleast 2 members are required',400);
+
+        return this.ChatRepo.create({
+            type:'General',
+            name,
+            members,
+            workspaceId
+        });
 
     }
 
 
     async getUserChats(userId) {
 
+        if (!userId) throw new ErrorHandler("User ID is required", 400);
+
+        return this.chatRepo.getUserChats(userId);
+
     }
 
     async addMember(chatId, userId) {
 
+        const ischat = await this.ChatRepo.findById({chatId});
+        if(!ischat) throw ErrorHandler('ChatRoom not found',404)
+
+
+        return this.ChatRepo.addMember({
+            chatId,
+            userId
+        });
     }
 
     // Remove member
     async removeMember(chatId, userId) {
 
+        const ischat = await this.ChatRepo.findById({chatId});
+        if(!ischat) throw ErrorHandler('ChatRoom not found',404)
+
+
+        return this.ChatRepo.removeMember({
+            chatId,
+            userId
+        });
     }
 
     // Send message
@@ -38,12 +68,20 @@ class Chatservice{
 
      // Fetch messages
     async getMessages(chatId) {
+        const chat = await this.chatRepo.findById(chatId);
+        if (!chat) throw new ErrorHandler("Chat not found", 404);
 
+        return this.messageRepo.getMessages(chatId);
     }
 
     // Mark message as read
     async markAsRead(messageId, userId) {
+        const msg = await this.messageRepo.findById(messageId);
+        if (!msg) throw new ErrorHandler("Message not found", 404);
 
+        return this.messageRepo.markAsRead(messageId, userId);
     }
 
 }
+
+export default Chatservice;
